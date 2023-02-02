772e1e1fa1fcf6b773822101a8728b5ae6fedbd9
Atharv <atharvastaputre5@gmail.com>
2023-02-02 14:53:15 +0530

    Commited Binary search

---
+++ Binary.java
@@ -0,0 +1,13 @@
+class Binary 
+{
+      public static void main (String args [])
+      {
+            /*
+
+                  CODE
+
+            */
+
+            System.out.println("Binary Search Code");
+      }
+}
