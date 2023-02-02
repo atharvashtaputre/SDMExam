6dc7fe6e12dd1a6319f265d2e4241907d220de9a
Atharv <atharvastaputre5@gmail.com>
2023-02-02 14:51:37 +0530

    Linear Search commited

---
+++ Linear.java
@@ -0,0 +1,13 @@
+class Linear 
+{
+      public static void main (String args [])
+      {
+            /*
+
+                  CODE
+
+            */
+
+            System.out.println("Linear Search Code");
+      }
+}
